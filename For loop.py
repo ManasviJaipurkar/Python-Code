d3470865309cfc1335a13d88e7720eb66b72d7cf
Manasvi Jaipurkar <106508591+ManasviJaipurkar@users.noreply.github.com>
2025-12-19 19:12:18 +0530

    Add files via upload

---
+++ For loop.py
@@ -0,0 +1,31 @@
+# Basic for Loop
+for i in range(5):
+    print(i)
+
+# 2 for Loop with Start and Stop
+for i in range(1, 6):
+    print(i)
+
+# 3 for Loop with Step Value
+for i in range(0, 10, 2):
+    print(i)
+
+# 4 Loop Through a List
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+# 5 Loop Through a String
+name = "Python"
+
+for ch in name:
+    print(ch)
+
+# 6 Sum of Numbers Using for Loop
+total = 0
+
+for i in range(1, 6):
+    total += i
+
+print("Sum is:", total)
