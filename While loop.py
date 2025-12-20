81d3e54edeef612b2c2a87818e9e7e12f13e6bfb
Manasvi Jaipurkar <106508591+ManasviJaipurkar@users.noreply.github.com>
2025-12-20 19:49:11 +0530

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,32 @@
+# 1 Basic while Loop
+
+i = 1
+
+while i <=5:
+    print(i)
+    i += 1
+
+# 2 Print Numbers from 1 to 10
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+
+# 3 Sum of Numbers Using while Loop
+i = 1
+total = 0
+
+while i <= 5:
+    total += i
+    i += 1
+
+print("Sum is:", total)
+
+# 4.Even Numbers Using while Loop
+i = 1
+
+while i <= 10:
+    if i % 2 == 0:
+        print(i)
+    i += 1
